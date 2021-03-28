c7bcef9b52c27c977268eee90c1dddb6499b11ee
Jaison Miller <jaison.miller@gmail.com>
2021-03-28 12:47:21 +0100

    interface files added
    Built in Material UI - files to support the usage of the API

---
+++ interface/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import RobotInterface from "./robot-interface";
+
+ReactDOM.render(<RobotInterface />, document.querySelector("#root"));

8c7d8b2d9d7dfaf3fc1c488947582f4ba1110a2c
Jaison Miller <jaison.miller@gmail.com>
2021-03-29 21:17:30 +0100

    Delete interface directory

--- interface/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import RobotInterface from "./robot-interface";
-
-ReactDOM.render(<RobotInterface />, document.querySelector("#root"));
